a92a31c49403491da844eb99f43179e7c2436227
ZetOmega <70434161+ZetOmega@users.noreply.github.com>
2022-04-05 18:26:06 +0200

    add better tooltip colors

--- src/main/java/net/philippklei/titano/mixin/ExampleMixin.java
+++ src/main/java/net/philippklei/titano/mixin/ExampleMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		TitanoMod.LOGGER.info("This line is printed by an example mod mixin!");
+		TitanoMod.LOGGER.info("This line is printed by the TitanoMod mixin!");
 	}
 }
